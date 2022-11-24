c83360cc0eab0512bb91be86ec2f211429cb39d2
simon <simon.kvasnovsky@gmail.com>
2022-11-24 09:54:50 +0100

    Add Dots to b.

--- LinearFunction.py
+++ LinearFunction.py
@@ -66,7 +66,7 @@ class LinearFunction(Scene):
 
        # INY ZAPIS PRE ZISKANIE SURADNICE BODU bod2 = Dot(np.array([0, 0, 0]), color=YELLOW)
         
-        konkretnyGrafLFnula = suradnicovaOs.plot(lambda y: 0*y+0, x_range=[-4,4])
+        konkretnyGrafLFnula = suradnicovaOs.plot(lambda y: 0*y+0, x_range=[-8,8])
        
 
         konkretnyVzorecLFnula = MathTex('{f: y = 0x+0}', font_size=50).to_edge(UL).set_color(YELLOW)
@@ -134,9 +134,54 @@ class LinearFunction(Scene):
         konkretnyVzorecLFminus3[0][5].set_color(RED)
         konkretnyVzorecLFminus3[0][6].set_color(RED)
 
+
+        GrafLFnula = suradnicovaOs.plot(lambda y: -3*y-0, x_range=[-4,4])
+        Vzorecnula = MathTex('{f: y = -3x-0}', font_size=50).to_edge(UL).set_color(YELLOW)
+        Vzorecnula[0][8].set_color(RED)
+        BodNulab = Dot(suradnicovaOs.coords_to_point(0,0), color=YELLOW)
+
+        GrafLFminus1b = suradnicovaOs.plot(lambda y: -3*y-1, x_range=[-4,4])
+        Vzorecminus1b = MathTex('{f: y = -3x-1}', font_size=50).to_edge(UL).set_color(YELLOW)
+        Vzorecminus1b[0][8].set_color(RED)
+        Bodminus1b = Dot(suradnicovaOs.coords_to_point(0,-1), color=YELLOW)
+      
+
+        GrafLFminus2b = suradnicovaOs.plot(lambda y: -3*y-2, x_range=[-4,4])
+        Vzorecminus2b = MathTex('{f: y = -3x-2}', font_size=50).to_edge(UL).set_color(YELLOW)
+        Vzorecminus2b[0][8].set_color(RED)
+        Bodminus2b = Dot(suradnicovaOs.coords_to_point(0,-2), color=YELLOW)
+
+
+        GrafLFminus3b = suradnicovaOs.plot(lambda y: -3*y-3, x_range=[-4,4])
+        Vzorecminus3b =  MathTex('{f: y = -3x-3}', font_size=50).to_edge(UL).set_color(YELLOW)
+        Vzorecminus3b[0][8].set_color(RED)
+        Bodminus3b = Dot(suradnicovaOs.coords_to_point(0,-3), color=YELLOW)
+
+        GrafLFplus1b = suradnicovaOs.plot(lambda y: -3*y+1, x_range=[-4,4])
+        Vzorecplus1b =  MathTex('{f: y = -3x+1}', font_size=50).to_edge(UL).set_color(YELLOW)
+        Vzorecplus1b[0][8].set_color(RED)
+        Bodplus1b = Dot(suradnicovaOs.coords_to_point(0,+1), color=YELLOW)
+
+
+        GrafLFplus2b = suradnicovaOs.plot(lambda y: -3*y+2, x_range=[-4,4])
+        Vzorecplus2b =  MathTex('{f: y = -3x+2}', font_size=50).to_edge(UL).set_color(YELLOW)
+        Vzorecplus2b[0][8].set_color(RED)
+        Bodplus2b = Dot(suradnicovaOs.coords_to_point(0,+2), color=YELLOW)
+
+
+        GrafLFplus3b = suradnicovaOs.plot(lambda y: -3*y+3, x_range=[-4,4])
+        Vzorecplus3b =  MathTex('{f: y = -3x+3}', font_size=50).to_edge(UL).set_color(YELLOW)
+        Vzorecplus3b[0][8].set_color(RED)
+        Bodplus3b = Dot(suradnicovaOs.coords_to_point(0,+3), color=YELLOW)
+
+
+
+
+
+
         
 
-        y_label = suradnicovaOs.get_y_axis_label("y").shift(UP*0.1, LEFT)
+        y_label = suradnicovaOs.get_y_axis_label("y").shift(UP, RIGHT)
         x_label = suradnicovaOs.get_x_axis_label("x").next_to(suradnicovaOs)
         grid_labels = VGroup(x_label, y_label)
 
@@ -145,7 +190,7 @@ class LinearFunction(Scene):
     
         self.play(Write(text1))    
         self.play(FadeOut(text1))                                                             
-        self.play(Write(suradnicovaOs), run_time=3)                                                       
+        self.play(Write(suradnicovaOs), run_time=3)                                                    
                                                                      
         
         self.play(Create(rastucaLF))  
@@ -221,6 +266,40 @@ class LinearFunction(Scene):
         self.play(ReplacementTransform(konkretnyGrafLFminus2, konkretnyGrafLFminus3 ))
         self.play(FadeOut(bod1Minus3, bod2Minus3, bodkociarka1Minus3, bodkociarka2Minus3))
 
+        self.play(FadeOut(arrow3, klesajucaVzorecText))
+
+        self.play(FadeIn(BodNulab))
+        self.play(ReplacementTransform(konkretnyVzorecLFminus3, Vzorecnula))
+        self.play(ReplacementTransform(konkretnyGrafLFminus3, GrafLFnula))
+
+        self.play(ReplacementTransform(BodNulab, Bodminus1b))
+        self.play(ReplacementTransform(Vzorecnula, Vzorecminus1b))
+        self.play(ReplacementTransform(GrafLFnula, GrafLFminus1b))
+
+        self.play(ReplacementTransform(Bodminus1b, Bodminus2b))
+        self.play(ReplacementTransform(Vzorecminus1b, Vzorecminus2b))
+        self.play(ReplacementTransform(GrafLFminus1b, GrafLFminus2b))
+
+        self.play(ReplacementTransform(Bodminus2b, Bodminus3b))
+        self.play(ReplacementTransform(Vzorecminus2b, Vzorecminus3b))
+        self.play(ReplacementTransform(GrafLFminus2b, GrafLFminus3b))
+
+
+        self.play(ReplacementTransform(Bodminus3b, Bodplus1b))
+        self.play(ReplacementTransform(Vzorecminus3b, Vzorecplus1b))
+        self.play(ReplacementTransform(GrafLFminus3b, GrafLFplus1b))
+
+
+        self.play(ReplacementTransform(Bodplus1b, Bodplus2b))
+        self.play(ReplacementTransform(Vzorecplus1b, Vzorecplus2b))
+        self.play(ReplacementTransform(GrafLFplus1b, GrafLFplus2b))
+
+        self.play(ReplacementTransform(Bodplus2b, Bodplus3b))
+        self.play(ReplacementTransform(Vzorecplus2b, Vzorecplus3b))
+        self.play(ReplacementTransform(GrafLFplus2b, GrafLFplus3b))
+
+        self.wait()
+      
 
 
 
